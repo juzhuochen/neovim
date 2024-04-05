012402b08a226f8e60fda4fb300739da6dfd0756
JuzhuoChen <juzhuochen@gmail.com>
2024-04-06 01:21:10 +0800

    fix somme bugs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,9 @@
 --
 return {
+  { import = "plugins.lsp"},
   { import = "plugins.theme" },
   { import = "plugins.common"},
   { import = "plugins.treesitter"},
-  { import = "plugins.lsp"},
 
 }
 
---
+++ lua/plugins/lsp/Clangd.lua
@@ -0,0 +1,14 @@
+return {
+	"neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            on_attach = on_attach
+            lspconfig.clangd.setup({
+            cmd = {"clangd"},
+
+            filetypes = {"c", "cpp", "h", "hpp","cuda"},
+
+            })
+        end
+      }
+
