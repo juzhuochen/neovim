c4bb3ff07247496f4b21ecde838032fa9f197e60
juzhuochen <juzhuochen@gmail.com>
2024-06-22 21:09:42 +0800

    add treansparent apperence for nvim

--- lua/plugins/theme/kanagawa.lua
+++ lua/plugins/theme/kanagawa.lua
@@ -2,6 +2,32 @@ return {
   "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
+    -- Default options:
+    require('kanagawa').setup({
+      compile = false,
+      undercurl = true,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = vim.g.transparent_enabled,
+      dimInactive = false,
+      terminalColors = true,
+      colors = {
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors)
+        return {}
+      end,
+      theme = "wave",
+      background = {
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+      },
+    })
+
     vim.cmd.colorscheme("kanagawa-wave")
   end,
 }
