560a5ad88e0cf30befc3508ca1afa183362b593c
juzhuochen <juzhuochen@gmail.com>
2024-09-21 17:01:34 +0800

    bug fix for mason

--- lua/plugins/common/mason-lspconfig.lua
+++ lua/plugins/common/mason-lspconfig.lua
@@ -2,27 +2,11 @@ return{
   "williamboman/mason-lspconfig.nvim",
   dependencies = { "mason.nvim"},
   config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = {"lua_ls"},
-    })
-
+    require("mason-lspconfig").setup()
     require("mason-lspconfig").setup_handlers({
       function (server_name)
         require("lspconfig")[server_name].setup ({})
       end,
-
-      ["clangd"] = function()
-        local lspconfig = require("lspconfig")
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        lspconfig.clangd.setup({
-          capabilities = capabilities,
-          on_attach = on_attach ,
-          cmd = {"clangd"},
-          filetypes = {"c","cpp","cc","h","hpp","cu"},
-
-        })
-      end,
-      --
     })
   end,
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { import = "plugins.common"},
   { import = "plugins.theme" },
-
 }
+
+
 
