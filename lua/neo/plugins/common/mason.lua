95a80a7e772de9323ad0ab0221806409165b9856
juzhuochen <juzhuochen@gmail.com>
2024-11-06 14:13:07 +0800

    add ui icons

--- lua/neo/plugins/common/mason.lua
+++ lua/neo/plugins/common/mason.lua
@@ -1,6 +1,12 @@
 return {
     "williamboman/mason.nvim",
     config = function()
-        require("mason").setup()
+        require("mason").setup({
+          ui = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        })
     end,
 }
