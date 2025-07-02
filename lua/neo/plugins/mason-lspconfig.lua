173a29e9ca488b7a0a5b9259b889a1a8f7ec4362
Juzhuo Chen <juzhuochen@gmail.com>
2025-07-02 09:50:15 +0800

    feature: improve mason-lspconfig setting

--- lua/neo/plugins/mason-lspconfig.lua
+++ lua/neo/plugins/mason-lspconfig.lua
@@ -1,12 +1,21 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { 
-		"lua_ls",
-	},
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = { 
+      "lua_ls",
     },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+  config = function ()
+    require("mason-lspconfig").setup() {
+      automatic_enable = {
+        "lua_ls",
+        "vimls",
+        "python"
+      }
+    }
+  end
 }
